01fc752dadd33b42eabb1493386d45f6d0eb8d1e
U180709061 <hakkicanbuluc@gmail.com>
2022-08-31 15:25:39 +0300

    added check the internet

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:mvrg_app/ui/homepage/home_page.dart';
 import 'package:mvrg_app/viewmodel/user_model.dart';
@@ -5,24 +6,61 @@ import 'package:provider/provider.dart';
 
 import '../ui/login/login_page.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  bool connection = true;
+
+  @override
+  void initState() {
+    super.initState();
+    checkIntConnection().then((value) {
+      setState(() {
+        connection = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final UserModel _userModel = Provider.of<UserModel>(context, listen: true);
-    if (_userModel.state == ViewState.idle) {
-      if (_userModel.user == null) {
-        return LoginPage();
+    if (connection) {
+      if (_userModel.state == ViewState.idle) {
+        if (_userModel.user == null) {
+          return LoginPage();
+        } else {
+          return const HomePage();
+        }
       } else {
-        return const HomePage();
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
       }
     } else {
       return const Scaffold(
         body: Center(
-          child: CircularProgressIndicator(),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "İnternet bağlantın olmadan uygulamayı kullanamazsın. "
+              "İnternet bağlantını kontrol et.",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
         ),
       );
     }
+  }
+
+  Future<bool> checkIntConnection() async {
+    return await (Connectivity().checkConnectivity()) !=
+        ConnectivityResult.none;
   }
 }
